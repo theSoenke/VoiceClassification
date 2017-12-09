f36aec90134d88c1356fd3c9e1da671082c029bf
theSoenke <theSoenke@users.noreply.github.com>
2017-12-09 14:23:19 +0100

    Read train data

--- common-voice.py
+++ common-voice.py
@@ -1,15 +1,20 @@
 import os
+import csv
 import tensorflow as tf
 import numpy as np
+from numpy import genfromtxt
 
 
-def preprocessing(path):
-    print(path)
+def read_csv(path):
+    data = genfromtxt(path, delimiter=',', skip_header=1, dtype=None)
+    return data
 
 
 def main():
-    path = os.environ['DATASET']
-    preprocessing(path)
+    base_path = os.environ['DATASET']
+    train_path = os.path.join(base_path, "cv-valid-train.csv")
+    train_labels = read_csv(train_path)
+    print(train_labels)
 
 
 if __name__ == "__main__":
