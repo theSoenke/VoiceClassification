12ead3bb267d5667a9a3624249c210e00a718f89
theSoenke <theSoenke@users.noreply.github.com>
2017-12-17 00:11:44 +0100

    Parallelize feature extraction

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-# common-voice
+# VoiceClassification
+
+Voice classification based on the [Common Voice](https://voice.mozilla.org/data) dataset
 
 ## Install
-1. `python3 -m pip install --user virtualenv`
-2. `python3 -m virtualenv env`
-3. `source env/bin/activate`
-4. `pip install -r requirements.txt`
+1. `python3 -m pip install --user pipenv`
+2. `pipenv shell`
+3. `pipenv install`
 
-## Run
-1. Extract data `python3 data_extraction.py --data ~/Downloads/cv_corpus_v1`
-2. Train `python3 train.py`
+## Train
+1. Run `pipenv shell`
+2. Extract data `python3 data_extraction.py --data ~/Downloads/cv_corpus_v1`
+3. Train `python3 train.py`
--- data_extraction.py
+++ data_extraction.py
@@ -4,6 +4,7 @@ import glob
 import librosa
 import argparse
 import pandas as pd
+import concurrent.futures
 
 target_dict = {
     "male": 0,
@@ -11,26 +12,35 @@ target_dict = {
 }
 
 
-def extract_track_features(base_path, track_paths, gender):
+def extract_track_feature(path, index):
+    print("Extracting ", path)
+    y, sr = librosa.load(path)
+    mfcc = librosa.feature.mfcc(y=y, sr=sr, hop_length=HOP_LENGTH, n_mfcc=13)
+    spectral_center = librosa.feature.spectral_centroid(
+        y=y, sr=sr, hop_length=HOP_LENGTH)
+    chroma = librosa.feature.chroma_stft(y=y, sr=sr, hop_length=HOP_LENGTH)
+    spectral_contrast = librosa.feature.spectral_contrast(
+        y=y, sr=sr, hop_length=HOP_LENGTH)
+
+    feature_matrix = np.zeros((TIME_SERIES_LENGTH, MERGED_FEATURES_SIZE))
+    feature_matrix[:, 0:13] = mfcc.T[0:TIME_SERIES_LENGTH, :]
+    feature_matrix[:, 13:14] = spectral_center.T[0:TIME_SERIES_LENGTH, :]
+    feature_matrix[:, 14:26] = chroma.T[0:TIME_SERIES_LENGTH, :]
+    feature_matrix[:, 26:33] = spectral_contrast.T[0:TIME_SERIES_LENGTH, :]
+    return (feature_matrix, index)
+
+
+def extract_features(base_path, track_paths, gender):
     data = np.zeros(
         (len(track_paths), TIME_SERIES_LENGTH, MERGED_FEATURES_SIZE))
     classes = []
-    for i, track_path in enumerate(track_paths):
-        print("Extracting ", track_path)
-        classes.append(target_dict[gender[i]])
-
-        y, sr = librosa.load(os.path.join(base_path, track_path))
-        mfcc = librosa.feature.mfcc(y=y, sr=sr, hop_length=HOP_LENGTH, n_mfcc=13)
-        spectral_center = librosa.feature.spectral_centroid(
-            y=y, sr=sr, hop_length=HOP_LENGTH)
-        chroma = librosa.feature.chroma_stft(y=y, sr=sr, hop_length=HOP_LENGTH)
-        spectral_contrast = librosa.feature.spectral_contrast(
-            y=y, sr=sr, hop_length=HOP_LENGTH)
-
-        data[i, :, 0:13] = mfcc.T[0:TIME_SERIES_LENGTH, :]
-        data[i, :, 13:14] = spectral_center.T[0:TIME_SERIES_LENGTH, :]
-        data[i, :, 14:26] = chroma.T[0:TIME_SERIES_LENGTH, :]
-        data[i, :, 26:33] = spectral_contrast.T[0:TIME_SERIES_LENGTH, :]
+    futures = []
+    with concurrent.futures.ProcessPoolExecutor(8) as executor:
+        for i, track in enumerate(track_paths):
+            classes.append(target_dict[gender[i]])
+            future = executor.submit(extract_track_feature, os.path.join(base_path, track), i)
+    for future in concurrent.futures.as_completed(futures):
+        data[future[1]] = future[0]
 
     return data, np.array(classes)
 
@@ -44,7 +54,7 @@ def save_features(features, classes, name):
         np.save(f, classes)
 
 
-def extract_features():
+def prepare_data():
     colums = ['filename', 'gender']
     train_data_gender = pd.read_csv(os.path.join(
         DATA_PATH, "cv-valid-train.csv"), usecols=colums)
@@ -53,7 +63,7 @@ def extract_features():
 
     train_tracks = train_data_gender['filename'].tolist()
     train_labels = train_data_gender['gender'].tolist()
-    train_features, train_labels = extract_track_features(
+    train_features, train_labels = extract_features(
         DATA_PATH,
         train_tracks,
         train_labels)
@@ -70,4 +80,4 @@ if __name__ == "__main__":
     DATA_PATH = FLAGS.data
     MERGED_FEATURES_SIZE = 33
 
-    extract_features()
+    prepare_data()
