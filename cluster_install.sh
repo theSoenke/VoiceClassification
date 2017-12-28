2368b22e5172a086f578a1dda5f1734e6189f8a1
theSoenke <theSoenke@users.noreply.github.com>
2017-12-28 12:53:19 +0100

    Add slurm start script

--- cluster_install.sh
+++ cluster_install.sh
@@ -11,15 +11,13 @@ cmake ../
 cmake --build .
 cmake -DCMAKE_INSTALL_PREFIX=$WORK/llvm -P cmake_install.cmake
 
-# Install dependencies
+# Install Python dependencies
 export PATH=$PATH:$HOME/.local/bin
 export PATH=$PATH:$WORK/llvm/bin
 export CPATH=$CPATH:$WORK/llvm/include
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$WORK/llvm/lib
 
-module unload env
-module load /sw/BASE/env/2017Q1-gcc-openmpi /sw/BASE/env/cuda-8.0.44_system-gcc
-python3 -m pip install --user pipenv
-cd $HOME/VoiceClassification
-pipenv shell
-pipenv install
+module unload env; module load /sw/BASE/env/2017Q1-gcc-openmpi /sw/BASE/env/cuda-8.0.44_system-gcc
+wget https://pypi.python.org/packages/e3/d3/4a356db5b6a2c9dcb30011280bc065cf51de1e4ab5a5fee44eb460a98449/tensorflow_gpu-1.4.1-cp36-cp36m-manylinux1_x86_64.whl#md5=b5ceb9705a8bbf57d854266a882c5757
+python3 -m pip install --user tensorflow_gpu-1.4.1-cp36-cp36m-manylinux1_x86_64.whl
+python3 -m pip install --user --upgrade pip librosa pandas keras
---
+++ slurm.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# one is no fit for the job! You can do that either in the sbatch
+# command line or here with the other settings.
+#SBATCH --job-name=test_tensorflow
+#SBATCH --partition=gpu --qos=gpu
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=2
+#SBATCH --time=01:00:00
+# Never forget that! Strange happenings ensue otherwise.
+#SBATCH --export=NONE
+
+set -e # Good Idea to stop operation on first error.
+
+source /sw/batch/init.sh
+
+# Load environment modules for your application here.
+
+module unload env
+module load /sw/BASE/env/2017Q1-gcc-openmpi /sw/BASE/env/cuda-8.0.44_system-gcc
+
+# Actual work starting here. You might need to call
+# srun or mpirun depending on your type of application
+# for proper parallel work.
+# Example for a simple command (that might itself handle
+# parallelisation).
+echo "Hello World! I am $(hostname -s) greeting you!"
+echo "Also, my current TMPDIR: $TMPDIR"
+
+echo "nvidia-smi:"
+
+srun bash -c 'nvidia-smi'
+
+# Let's pretend our started processes are working on a
+# predetermined parameter set, looking up their specific
+# parameters using the set number and the process number
+# inside the batch job.
+export PARAMETER_SET=42
+# Simplest way to run an identical command on all allocated
+# cores on all allocated nodes. Use environment variables to
+# tell apart the instances.
+srun bash -c 'echo "process $SLURM_PROCID \
+(out of $SLURM_NPROCS total) on $(hostname -s) \
+parameter set $PARAMETER_SET"'
+
+srun bash -c 'CUDA_VISIBLE_DEVICES=$SLURM_PROCID LD_LIBRARY_PATH=/sw/compiler/cuda-8.0.44/lib64:$HOME/cuda/lib64/ python3 $HOME/train.py'
