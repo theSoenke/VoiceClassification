4757a0c1aa8649fcf198d0030b0ffc66079a3bc9
theSoenke <theSoenke@users.noreply.github.com>
2018-01-20 17:32:57 +0100

    Add pipenv setup

--- cluster_install.sh
+++ cluster_install.sh
@@ -20,4 +20,9 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$WORK/llvm/lib
 module unload env; module load /sw/BASE/env/2017Q1-gcc-openmpi /sw/BASE/env/cuda-8.0.44_system-gcc
 wget https://pypi.python.org/packages/e3/d3/4a356db5b6a2c9dcb30011280bc065cf51de1e4ab5a5fee44eb460a98449/tensorflow_gpu-1.4.1-cp36-cp36m-manylinux1_x86_64.whl#md5=b5ceb9705a8bbf57d854266a882c5757
 python3 -m pip install --user tensorflow_gpu-1.4.1-cp36-cp36m-manylinux1_x86_64.whl
-python3 -m pip install --user --upgrade pip librosa pandas keras
+python3 -m pip install --user --upgrade pipenv
+cd $WORK
+git clone https://github.com/thesoenke/VoiceClassification
+cd VoiceClassification
+pipenv shell
+pipenv install
